7688720a50c969f6a516f0e4ee0242ce2edc307e
bpinsard <basile.pinsard@gmail.com>
2024-01-10 11:28:47 -0500

    fix test data loading and packaging

--- gradunwarp/core/tests/test_regression.py
+++ gradunwarp/core/tests/test_regression.py
@@ -1,3 +1,6 @@
+import os
+DATA_DIR = os.path.join(os.path.dirname(__file__), 'data')
+
 import numpy as np
 from numpy.testing import assert_equal, assert_array_equal, \
     assert_array_almost_equal, assert_almost_equal
@@ -6,7 +9,8 @@ from gradunwarp.core import coeffs, utils
 from gradunwarp.core.unwarp_resample import siemens_B, cart2sph
 
 def test_siemens_B():
-    gradfile = 'gradunwarp/core/tests/data/gradunwarp_coeffs.grad'
+    gradfile = os.path.join(DATA_DIR, 'gradunwarp_coeffs.grad')
+
     siemens_coeffs = coeffs.get_coefficients('siemens', gradfile)
     R0 = siemens_coeffs.R0_m  * 1000
 
@@ -14,7 +18,7 @@ def test_siemens_B():
     x, y ,z = np.meshgrid(vec, vec, vec)
     r, cos_theta, theta, phi = cart2sph(x, y, z)
 
-    ref_b = np.load('gradunwarp/core/tests/data/siemens_B_output.npz')
+    ref_b = np.load(os.path.join(DATA_DIR, 'siemens_B_output.npz'))
 
     for d in 'xyz':
         alpha_d = getattr(siemens_coeffs, f"alpha_{d}")
