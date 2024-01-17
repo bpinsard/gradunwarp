7af08292d810192caabefa411a68dc32b11b6f4e
bpinsard <basile.pinsard@gmail.com>
2024-01-16 20:07:50 -0500

    fix setup.py after rebase

--- setup.py
+++ setup.py
@@ -6,14 +6,5 @@ extensions and a legacy script.
 from setuptools import setup
 
 setup(
-    ext_modules=[
-        Extension(
-            'gradunwarp.core.{}_ext'.format(mod),
-            include_dirs=[get_include()],
-            sources=['gradunwarp/core/{}_ext.c'.format(mod)],
-            extra_compile_args=['-O3'],
-        )
-        for mod in ('interp3', 'legendre', 'transform_coordinates')
-    ],
     scripts=['gradunwarp/core/gradient_unwarp.py'],
 )
